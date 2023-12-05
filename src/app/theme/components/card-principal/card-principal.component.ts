ca8198102fcdefd1dc729fda36c5119117317584
Fernando Gurgel <gurgelff@gmail.com>
2023-12-05 20:25:55 -0300

    fix(components): Insere fecha-parêntese em rotation no Card Principal

--- src/app/theme/components/card-principal/card-principal.component.ts
+++ src/app/theme/components/card-principal/card-principal.component.ts
@@ -32,7 +32,7 @@ export class CardPrincipalComponent {
     const rotation =
       currentVariation === EnumVariation.HIGHER
         ? 'rotate(180 8.5 5.5)'
-        : 'rotate(0 8.5 5.5';
+        : 'rotate(0 8.5 5.5)';
 
     const fill =
       currentVariation !== EnumVariation.HIGHER ? '#FF7474' : '#A3FCB6';
