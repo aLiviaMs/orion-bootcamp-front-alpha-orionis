aba4e1ed0afab8cee358a517ee61e770d746ea7a
Fernando Gurgel <gurgelff@gmail.com>
2023-12-06 01:51:17 -0300

    fix(components): Reverte cores das setas para as originais no Card Principal

--- src/app/theme/components/card-principal/card-principal.component.ts
+++ src/app/theme/components/card-principal/card-principal.component.ts
@@ -35,7 +35,7 @@ export class CardPrincipalComponent {
         : 'rotate(0 8.5 5.5';
 
     const fill =
-      currentVariation !== EnumVariation.HIGHER ? '#FF7474' : '#A3FCB6';
+      currentVariation !== EnumVariation.HIGHER ? '#A3FCB6' : '#FF7474';
 
     const height = currentVariation !== EnumVariation.HIGHER ? '35px' : '17px';
     const width = currentVariation !== EnumVariation.HIGHER ? '25px' : '17px';
